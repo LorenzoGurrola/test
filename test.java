c53aa05cb5bfbfc2e55c07bd92dd3b5172cae81d
LorenzoGurrola <lorenzogurrola521@gmail.com>
2024-09-29 14:03:28 -0500

    complete

--- test.java
+++ test.java
@@ -1,29 +1,25 @@
-using java
+import java.util.*;
 
-public static void main(String[] args) {
+public class test {
 
-    List<String> myList = new ArrayList<>();
-
-    myList.add("Bret");
-
-    myList.add("Suhas");
-
-    myList.add("Shilad");
-
-    myList.add("Susan");
-
-
-
-    for(String name : myList){
-
-        if (name.equals("Suhas") || name.equals("Shilad")){
-
-            myList.remove(name);
-
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        myList.add("Bret");
+        myList.add("Suhas");
+        myList.add("Shilad");
+        myList.add("Susan");
+        List<String> removeList = new ArrayList<>();
+        for(String name : myList){
+            if (name.equals("Suhas") || name.equals("Shilad")){
+                removeList.add(name);
+            }
         }
-
+        for(String name : removeList){
+                myList.remove(name);
+        }
+    
+        myList.stream().forEach(System.out::println);
     }
 
-    myList.stream().forEach(System.out::println);
 
 }
